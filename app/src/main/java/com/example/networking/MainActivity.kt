057866c335c7ac7e690af201fba24f92f83de670
WINDOWS 10 <mirzayevshaxzod9@gmail.com>
2022-06-10 21:10:53 +0500

    ...

--- app/src/main/java/com/example/networking/MainActivity.kt
+++ app/src/main/java/com/example/networking/MainActivity.kt
@@ -1,31 +1,50 @@
 package com.example.networking
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build.VERSION.SDK
-import android.os.Build.VERSION.SDK_INT
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.Volley
 import com.example.networking.databinding.ActivityMainBinding
 import com.example.networking.utils.NetworkHelper
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var networkHelper: NetworkHelper
+    lateinit var requestQueue: RequestQueue
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        networkHelper= NetworkHelper(this)
+        networkHelper = NetworkHelper(this)
 
-        if (networkHelper.isNetworkConnected()){
-            binding.tv.text="Connected"
-        }else{
-            binding.tv.text="Disconnected"
+        if (networkHelper.isNetworkConnected()) {
+          requestQueue=Volley.newRequestQueue(this)
+
+             fetchImageLoad()
+
+//            binding.tv.text = "Connected"
+        } else {
+
+            binding.tv.text = "Disconnected"
         }
 
     }
+
+    private fun fetchImageLoad() {
+        val imageRequest=ImageRequest("https://www.instagram.com/p/CF2WtGDA_GE/",
+            { response -> binding.imageView.setImageBitmap(response) },0,0,
+            ImageView.ScaleType.CENTER_CROP,Bitmap.Config.ARGB_8888
+        ) { error -> binding.tv.text = error?.message }
+        requestQueue.add(imageRequest)
+    }
+
 
 //    private fun isNetworkConnected():Boolean{
 //        val connectivityManager=getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
